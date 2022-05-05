ca50880265283f916d03745346049ece3e924f6a
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-05-05 10:16:27 +0200

    Rename mycomputer to my_computer.
    Necessary for style.

--- Using_python_to_interact_with_the_operating_system/Regular_expressions/Advanced_regex/AdvancedRegex.py
+++ Using_python_to_interact_with_the_operating_system/Regular_expressions/Advanced_regex/AdvancedRegex.py
@@ -4,7 +4,7 @@ import re
 
 # Using regex to find a PID...
 
-log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"
+log = "July 31 07:51:48 my_computer bad_process[12345]: ERROR Performing package upgrade"
 regex = r"\[(\d+)\]"
 result = re.search(regex, log)
 print(result[1])
