a25340cfe3ebaedc5a6b10d9f8ea47360ed86ae7
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-05-05 10:13:27 +0200

    This commit fixes merge conflicts and errors.
    Necessary to get the remote and local
    in sync.

---
+++ Using_python_to_interact_with_the_operating_system/Regular_expressions/Advanced_regex/AdvancedRegex.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env Python3
+
+import re
+
+# Using regex to find a PID...
+
+log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"
+regex = r"\[(\d+)\]"
+result = re.search(regex, log)
+print(result[1])
+
+
