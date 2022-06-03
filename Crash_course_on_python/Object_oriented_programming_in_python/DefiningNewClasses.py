9285c79558d121f3b28bc6826844b3f5284fe365
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-06-03 09:23:27 +0200

    Initial commit for three files.
    Initial commit for DefiningNewClasses.py, CreatFile.py, and
    Parameters.py.

---
+++ Crash_course_on_python/Object_oriented_programming_in_python/DefiningNewClasses.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# The point of OOP is defining real world concepts in a way that the computer understands...
+
+class Apple:
+    """Class names
+    should start with a
+    capital letter
+    according to PEP8
+    """
+    pass
+
