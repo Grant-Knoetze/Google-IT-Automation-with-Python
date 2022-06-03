877f748d65092bb67fa8f4bed8ccba3f87c536d3
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-06-03 09:29:08 +0200

    Assign attributes to the Apple class.
    This necessary as I progress through the object oriented programming
    lesson as part of the Google IT Automation with Python Professional
    Certificate.

--- Crash_course_on_python/Object_oriented_programming_in_python/DefiningNewClasses.py
+++ Crash_course_on_python/Object_oriented_programming_in_python/DefiningNewClasses.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
-# The point of OOP is defining real world concepts in a way that the computer understands...
+# The point of OOP is defining real world concepts in a way that the computer understands.
 
 class Apple:
-    """Class names
-    should start with a
-    capital letter
-    according to PEP8
+    """Class names should start
+    with a capital letter
+    according to PEP8.
     """
-    pass
+    pass  # Use pass keyword to show that body is empty.
 
+
+class Apple:
+    """After defining the class we
+    can now assign attributes to it."""
+    color = " "
+    flavor = " "
+
+    
