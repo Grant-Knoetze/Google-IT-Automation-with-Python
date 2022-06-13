aa870b93a0653a9e951b0662648bb0aef7e44272
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-06-13 12:49:00 +0200

    Initial commit for Constructors.py.
    Initial commit for Constructors.py which includes the defining of the Apple class. as well as the initialization of an instance of the class including instance variables color and flavor.

---
+++ Crash_course_on_python/Object_oriented_programming_in_python/Constructors.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+class Apple:
+    """The constructor of a class
+    is the method that is called when the
+    name of the class is called, it
+    is always named __init__"""
+    def __init__(self, color, flavor):
+        self.color = color
+        self.flavor = flavor
+        
---
+++ Using_python_to_interact_with_the_operating_system/Subprocesses/DateExample.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+subprocess.run(["date"])
+
+tree1 = "palm"
+tree2 = "Queen Palm"
+
+print(f'A {tree1} is smaller than a {tree2}')
+
+
+frog1 = "Tree Frog"
+frog2 = "Brown Tree Frog"
+
+print(f'A {frog2} is bigger than a {frog1}')
+
