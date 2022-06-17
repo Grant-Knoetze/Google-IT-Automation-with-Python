454d71870ac9363a7518c681fcbd0b49c83d5f77
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-06-17 20:50:50 +0200

    Define __str__ method.
    Define special __str__ method.

--- Crash_course_on_python/Object_oriented_programming_in_python/Constructors.py
+++ Crash_course_on_python/Object_oriented_programming_in_python/Constructors.py
@@ -4,13 +4,26 @@ class Apple:
     """The constructor of a class
     is the method that is called when the
     name of the class is called, it
-    is always named __init__"""
+    is always named __init__,
+    after which we define a
+    string method to return a
+    given instances attributes
+    in a string format."""
 
     def __init__(self, color, flavor):
         self.color = color
         self.flavor = flavor
 
+    def __str__(self):
+        return "This apple is {} and its flavor is {}".format(self.color, self.flavor)
+
 
 jonagold = Apple("red", "sweet")
 print(jonagold.color)
 print(jonagold.flavor)
+
+granny_smith = Apple("green", "sour")
+print(granny_smith.color)
+print(granny_smith.flavor)
+
+print(jonagold)
