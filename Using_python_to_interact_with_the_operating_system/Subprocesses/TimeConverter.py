c5ead99cabe5cc161f3ea2926d21f72a43472b1b
Grant-Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-06-19 16:57:55 +0200

    Initial commit for Subprocesses.
    All these scripts developed using Kali Linux
    on a virtual machine hosted on my main
    computer. This commit necessary to update
    this repository with these files.

---
+++ Using_python_to_interact_with_the_operating_system/Subprocesses/TimeConverter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+def to_seconds(hours, minutes, seconds):
+    """User provides the
+    input for hours, minutes,
+    seconds,
+    user prompted for
+    another conversion."""
+    return hours * 3600 + minutes * 60 + seconds
+
+
+print("Welcome to this time converter")
+
+cont = "y"
+while cont.lower() == "y":
+    hours = int(input("Enter the number of hours: "))
+    minutes = int(input("Enter the number of minutes: "))
+    seconds = int(input("Enter the number of seconds: "))
+
+    print("Thats {} seconds".format(to_seconds(hours, minutes, seconds)))
+    print()
+    cont = input("Do you want another conversion? [y to continue] ")
+
+print("Good bye!")
