b2d056a3d9e73075264c2226c4283128070085a2
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-07-19 13:03:36 +0200

    Add functionality to count CRON jobs.
    Add functionality for counting the number of times a user scheduled
    a CRON job using a dictionary, and the get method on the dictionary.

--- Using_python_to_interact_with_the_operating_system/Working_with_logs/check_cron.py
+++ Using_python_to_interact_with_the_operating_system/Working_with_logs/check_cron.py
@@ -4,10 +4,19 @@ import re
 import sys
 
 logfile = sys.argv[1]
+usernames = {}
+
 with open(logfile) as f:
+    """Gets the user name of any user who scheduled a CRON job,
+    then counts the number of times that user has done so."""
     for line in f:
         if "CRON" not in line:
             continue
             pattern = r"USER \((\w+)\)$"
             result = re.search(pattern, line)
-            print(result[1])
+            if result is None:
+                continue
+            name = result[1]
+            usernames[name] = usernames.get(name, 0) + 1
+
+print(usernames)
