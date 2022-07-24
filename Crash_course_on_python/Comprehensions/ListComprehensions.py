9dc135d61c4c605b0d9cbb4c011574e2edbbf698
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-07-24 08:40:36 +0200

    Create test.py and modify Comprehensions.

--- Crash_course_on_python/Comprehensions/ListComprehensions.py
+++ Crash_course_on_python/Comprehensions/ListComprehensions.py
@@ -21,5 +21,18 @@ z = [x for x in range(1, 101) if x % 3 == 0]
 print(z)
 
 
+def larger_sum(lst1, lst2):
+    sum1 = [lst1 for i in lst1]
+    sum2 = [lst2 for n in lst2]
+    if sum1 >= sum2:
+        return lst1
+    else:
+        return lst2
+
+
+# Uncomment the line below when your function is done
+print(larger_sum([1, 9, 5], [2, 3, 7]))
+
+
 
 
