9ceb018657ca78437ff26928b28f869941bc4736
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-08-07 09:33:57 +0200

    Add example to dictionary comprehensions

--- Crash_course_on_python/Comprehensions/dictionaryComprehensions.py
+++ Crash_course_on_python/Comprehensions/dictionaryComprehensions.py
@@ -2,6 +2,17 @@
 
 def main():
     my_numbers = [1, 35, 514, 1235, 3456, 3, 2, 4]
+    my_letters = ["l", "k", "l", "j", "h", "a", "h", "h"]
+    my_letters_and_numbers = {}
+    for number in my_numbers:
+        for letter in my_letters:
+            if my_numbers.index(number) == my_letters.index(letter):
+                my_letters_and_numbers[number] = letter
+    print(my_letters_and_numbers)
+
+    my_letters_and_numbers2 = {number: letter for number in my_numbers for letter in my_letters
+                               if my_numbers.index(number) == my_letters.index(letter)}
+    print(my_letters_and_numbers2)
 
     # Creating a dictionary with the key as the number and value as the square of the number:
     my_dict = {}
