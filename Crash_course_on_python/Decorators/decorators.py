cc312e8e148efc0cb6736445f9b4e8557389e71b
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-08-28 09:58:56 +0200

    Modified.

--- Crash_course_on_python/Decorators/decorators.py
+++ Crash_course_on_python/Decorators/decorators.py
@@ -1,2 +1,29 @@
 #!/usr/bin/env python3
 
+import functools
+
+
+def hello_message_decorator(func):
+    """This is the hello message decorator"""
+
+    def wrapper():
+        """This is wrapper inside hello
+        message decorator"""
+
+        return ">>>>>> Wrapper: Hello! <<<<<<"
+    return wrapper
+
+
+@hello_message_decorator
+def hello_message():
+    """This is the hello message function
+    Using the @ decorator, control is passed to
+    the hello_message_decorator function"""
+    return "hello"
+
+print(hello_message())
+#message = hello_message_decorator(hello_message)
+#print(message())
+
+
+
