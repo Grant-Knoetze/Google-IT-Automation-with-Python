1d876464b2427bd636d9dfdc9dfe025843fcd94f
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-09-01 14:38:14 +0200

    Add functionality to raise error.
    Add an if statement to raise a value error if
    minlen < 1.

--- Using_python_to_interact_with_the_operating_system/Testing_in_python/raisingExceptions.py
+++ Using_python_to_interact_with_the_operating_system/Testing_in_python/raisingExceptions.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
+# Add check to check that minlen is at least 1, if not raise an error.
+
 def validate_user(username, minlen):
     if len(username) < minlen:
         return False
     if not username.isalnum():
         return False
+    if minlen < 1:
+        raise ValueError("minlen must be at least 1")
     return True
 
 
