100636ead65e4e3f9a4af353d4bc86bd4203b080
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-09-01 15:35:49 +0200

    Add assertRaises function.
    Add assertRaises function checking if ValueError is raised
    with invalid minlen value.

--- Using_python_to_interact_with_the_operating_system/Testing_in_python/testingForErrors.py
+++ Using_python_to_interact_with_the_operating_system/Testing_in_python/testingForErrors.py
@@ -2,7 +2,8 @@
 
 import unittest
 
-from validations import validate_user
+from raisingExceptions import validate_user
+
 
 class testValidateUser(unittest.TestCase):
     def test_valid(self):
@@ -13,5 +14,12 @@ class testValidateUser(unittest.TestCase):
 
     def test_invalid_characters(self):
         self.assertEqual(validate_user("invalid_user", 1), False)
+
+    def invalid_minlen(self):
+        """assertRaises ValueError for an invalid minlen,
+        assertRaises function from unittest takes the error
+        expected to be raised, function, and parameters."""
+        self.assertRaises(ValueError, validate_user, "user", -1)
+
 
 unittest.main()
