77a54f2b90eb20183dcd846216c2b9986dc5acc0
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-09-01 14:34:44 +0200

    Create raisingExceptions.py.

---
+++ Using_python_to_interact_with_the_operating_system/Testing_in_python/raisingExceptions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def validate_user(username, minlen):
+    if len(username) < minlen:
+        return False
+    if not username.isalnum():
+        return False
+    return True
+
+
+
+
+
+#print(validate_user("Grant", 0))
