0686b8fe3a8012b52bdbbff2917287097b2f8389
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-09-01 14:42:59 +0200

    Change position of statement to raise error.
    This if statement to raise value error
    needs to be higher in the function.

--- Using_python_to_interact_with_the_operating_system/Testing_in_python/raisingExceptions.py
+++ Using_python_to_interact_with_the_operating_system/Testing_in_python/raisingExceptions.py
@@ -3,16 +3,16 @@
 # Add check to check that minlen is at least 1, if not raise an error.
 
 def validate_user(username, minlen):
+    if minlen < 1:
+        raise ValueError("minlen must be at least 1")
     if len(username) < minlen:
         return False
     if not username.isalnum():
         return False
-    if minlen < 1:
-        raise ValueError("minlen must be at least 1")
     return True
 
 
 
 
 
-#print(validate_user("Grant", 0))
+print(validate_user("", -1))
