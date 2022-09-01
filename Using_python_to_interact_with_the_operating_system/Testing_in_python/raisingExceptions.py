731b0b1b7db14abad7aaf1e4f30e5d43bfcf222a
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-09-01 15:17:51 +0200

    Add assertion.
    Add assertion that raises assertion error if username
    not a string.

--- Using_python_to_interact_with_the_operating_system/Testing_in_python/raisingExceptions.py
+++ Using_python_to_interact_with_the_operating_system/Testing_in_python/raisingExceptions.py
@@ -3,6 +3,7 @@
 # Add check to check that minlen is at least 1, if not raise an error.
 
 def validate_user(username, minlen):
+    assert type(username) == str, "username must be a string"
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
     if len(username) < minlen:
@@ -11,4 +12,4 @@ def validate_user(username, minlen):
         return False
     return True
 
-# print(validate_user("jeffrey", 8))
+print(validate_user("Jake", 8))
