e37ffa15a796fbdee0bf3d767303a4fdbcff4375
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-10-12 13:39:01 +0200

    Add main() and initialize dictionaries and lists.
    Initialize two empty dictionaries and one empty list to
    store errors, error per user and user count. Add main()
    function that includes taking the log_file as a
    command line argument.

--- Using_python_to_interact_with_the_operating_system/Bash_scripting/find_errors.py
+++ Using_python_to_interact_with_the_operating_system/Bash_scripting/find_errors.py
@@ -3,13 +3,25 @@ import operator
 import re
 import sys
 
+dict_errors = {}
+dict_per_usr = {}
+user_count = []
+
 
 def log_errors():
     """Set regular expression to find lines containing ERROR"""
 
 
-with open("/var/log/syslog") as file:
+with open(log_file) as file:
     for line in file:
         info = re.findall(r"ticky: (?P<logtype>INFO|ERROR)", line)
 
 print(log_errors())
+
+
+def main():
+    log_file = sys.argv[1]
+
+
+if __name__ == "__main__":
+    main()
