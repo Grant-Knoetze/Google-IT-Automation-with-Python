70437affd80dc0bc8d357742828ba09c64ac9939
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-10-27 12:43:30 +0200

    Created a generator expression.
    Created a generator expression.

--- Advanced_python_concepts/generator2.py
+++ Advanced_python_concepts/generator2.py
@@ -5,11 +5,19 @@ def square_numbers(n):
         yield i ** 2
 
 
-x = square_numbers(5)
 
-for num in x:
+print(list(square_numbers(5)))
+#x = square_numbers(5)
+
+#for num in x:
+    #print(num)
+
+# Create generator function
+
+numbers_cubed = (n ** 3 for n in range(98))
+
+for num in numbers_cubed:
     print(num)
-    
-#print(x.__next__())
-#print(x.__next__())
-#print(x.__next__())
+# print(x.__next__())
+# print(x.__next__())
+# print(x.__next__())
