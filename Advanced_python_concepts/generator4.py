22fb52b55142f91f9cc77b1ddbea1d987074254b
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-11-12 12:57:50 +0200

    Create generator4.py.
    Add root, dirs, and files in dailysync.py.

---
+++ Advanced_python_concepts/generator4.py
@@ -0,0 +1,13 @@
+# Generator pipeline
+
+def get_odd_numbers_squared_and_ending_in_1():
+    """if number is odd, square that number,
+    if the number ends in 1, print"""
+    for n in range(1000):
+        if n % 2 != 0:
+            n **= 2
+            if n % 10 == 1:
+                print("Match found ---->{}".format(n))
+
+
+get_odd_numbers_squared_and_ending_in_1()
