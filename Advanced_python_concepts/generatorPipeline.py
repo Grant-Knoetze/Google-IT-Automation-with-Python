3798714f3f9bd03e5676ae35321b84cd9d79db0c
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-11-17 19:08:08 +0200

    Add function.
    Add square function, then call that function
    as part of the generatorPipeline.

--- Advanced_python_concepts/generatorPipeline.py
+++ Advanced_python_concepts/generatorPipeline.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
 def odd_filter(nums):
-    """Square the number"""
+    """Yield odd numbers only"""
     for num in nums:
         if num % 2 != 0:
             yield num
 
-# Generator pipeline takes odd_filter function 
-generator_pipeline = odd_filter(range(1000))
+
+def square(nums):
+    """Square the numbers"""
+    for num in nums:
+        num **= 2
+        yield num
+
+
+# Generator pipeline takes odd_filter function
+generator_pipeline = square(odd_filter(range(1000)))
+
 for n in generator_pipeline:
     print(n)
