3c20ce9a7fdc178fff7a213a6cc6f13de72c7eea
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-11-17 16:46:09 +0200

    Add odd filter func.
    Add odd filter function and generatorPipeline
    generator pipeline.

--- Advanced_python_concepts/generatorPipeline.py
+++ Advanced_python_concepts/generatorPipeline.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+def odd_filter(nums):
+    """Square the number"""
+    for num in nums:
+        if num % 2 != 0:
+            yield num
+
+# Generator pipeline takes odd_filter function 
+generator_pipeline = odd_filter(range(1000))
+for n in generator_pipeline:
+    print(n)
